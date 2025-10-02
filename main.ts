1af7d7c8571d008e43cf79755395beba7970d080
Brendan-Orourke <brendan.orourke@stu.ocsb.ca>
2025-10-02 13:14:28 -0400

    This program shows a checkmark when button a is pressed and an x when button b is pressed

--- main.ts
+++ main.ts
@@ -5,4 +5,27 @@
  * This program ...
 */
 
-basic.showString('Hello, World!')
+/* Copyright (c) 2025 MTHS All rights reserved
+*
+* Created by: Brendan O'Rourke
+* Created on: Sep 2025
+* This program turns an LED on and off.
+*/
+
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+
+input.onButtonPressed(Button.A, function () {
+    // turns pin 16 on
+    basic.showIcon(IconNames.Yes)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+})
+
+
+input.onButtonPressed(Button.B, function () {
+    // turns pin 16 off
+    basic.showIcon(IconNames.No)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+})
